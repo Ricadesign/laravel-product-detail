46f9af17de8a378677397a2ec1d4026105f752c3
Joel Pérez Alava <jperez@rica.design>
2021-03-01 14:28:11 +0100

    publish controller

--- src/LaravelProductDetailServiceProvider.php
+++ src/LaravelProductDetailServiceProvider.php
@@ -42,7 +42,8 @@ class LaravelProductDetailServiceProvider extends ServiceProvider
             __DIR__.'/../config/product.php' => config_path('product.php'),
             __DIR__.'/../resources/views' => resource_path('views/vendor/product'),
             __DIR__.'/../resources/assets/sass' => resource_path('sass/vendor/product'),
-        ], 'product[config-sass-view]');
+            __DIR__.'/ProductDetailController.php' => app_path('Http/Controllers/ProductDetailController.php'),
+        ], 'product[config-sass-view-controller]');
         $this->publishes([
             __DIR__.'/../database/migrations' => database_path('migrations'),
             __DIR__.'/Product.php' => app_path('Models/Product.php')
